526d143aafd3e02c540ba18fe70a5764b6b10e58
Amrita Datta <78852592+AmritaDatta@users.noreply.github.com>
2021-02-19 18:26:04 +0100

    Update HcmLcm.java

--- HcmLcm.java
+++ HcmLcm.java
@@ -1,14 +1,5 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package hcmlcm;
 
-/**
- *
- * @author I530311
- */
 public class HcmLcm {
     
     public static void main(String[] args) {
@@ -31,7 +22,5 @@ public class HcmLcm {
         hcf = num2;
         System.out.println("Hcf =" + hcf);
         System.out.println("Lcm =" + (a*b)/ hcf);
-    }
-    
-    
+    }  
 }
