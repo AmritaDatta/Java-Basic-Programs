a5a6375d11cbed8d368c1396aa53d1e8ce495057
Amrita Datta <78852592+AmritaDatta@users.noreply.github.com>
2021-02-20 03:18:39 +0100

    Add files via upload
    Java Program to check Leap Year

---
+++ LeapYear.java
@@ -0,0 +1,16 @@
+package leapyear;
+
+public class LeapYear {
+
+    public static void main(String[] args) {
+        
+        int year=2016;
+        if ((year%400==0) || (year%4==0 && year%100!=0)){
+            System.out.println("LeapYear");
+        }
+        else{
+            System.out.println("NOT LeapYear");
+        }
+    }
+    
+}
