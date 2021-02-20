964019e01e5ffe1341940136bd143854152b4457
Amrita Datta <78852592+AmritaDatta@users.noreply.github.com>
2021-02-20 16:25:25 +0100

    Add files via upload
    Find Duplicate Elements in an Array using Set Interface

---
+++ DuplicateElementsInArray.java
@@ -0,0 +1,25 @@
+package duplicateelementsinarray;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class DuplicateElementsInArray {
+
+    public static void main(String[] args) {
+        
+        int[] arr={2,4,5,3,2,5,6};
+        
+        //Set Interface; It can't store duplicate elements; we can't create object of Interface;
+        //Creating Refference of Set Interface
+        //<Integer> - Generics; s - refference;
+        //HashSet is a class, we can creat object of it;
+        
+        Set<Integer> s=new HashSet<>();
+        
+        for (int no : arr){
+            if (s.add(no)== false){
+                System.out.println(no);
+            }
+        }  
+    }
+}
